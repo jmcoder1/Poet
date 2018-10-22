9659a84ff8efb7e823cc86a24987f3f5ecafd3a1
Jojo Masala <jojomasala@protonmail.com>
2018-10-22 23:58:36 +0100

    opens gallery image and tries to retrieve uri

--- app/src/main/java/com/example/android/poet/PartnerEditorFragment.java
+++ app/src/main/java/com/example/android/poet/PartnerEditorFragment.java
@@ -1,47 +1,68 @@
 package com.example.android.poet;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
+import static android.app.Activity.RESULT_OK;
+
 public class PartnerEditorFragment extends Fragment{
+
+    private static final int GALLERY_CODE = 1;
+
+    private Uri mImageUri;
+    private CircleImageView partnerEditorImageView;
 
     public PartnerEditorFragment() {
 
     }
 
+    private View.OnTouchListener mTouchListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            openGallery();
+            return false;
+        }
+    };
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View rootView = inflater.inflate(R.layout.fragment_partner_profile_img, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_partner_editor_img, container, false);
 
-        CircleImageView partnerEditorImageView = (CircleImageView) rootView.findViewById(R.id.partner_editor_img);
+        partnerEditorImageView = (CircleImageView) rootView.findViewById(R.id.partner_editor_img);
+        partnerEditorImageView.setOnTouchListener(mTouchListener);
 
-        if(hasProfilePicture()) {
-            // TODO: Skip this away
-            //partnerProfileTextView.setVisibility(TextView.INVISIBLE);
-        } else {
-            //partnerProfileTextView.setVisibility(TextView.VISIBLE);
-            //partnerProfileImageView.setVisibility(View.INVISIBLE);
-            //partnerProfileTextView.setText(firstLetter);
-        }
-        // Return the rootView
         return rootView;
     }
 
-    /**
-     * Helper method to check if the user has added a profile picture
-     *
-     */
-    private boolean hasProfilePicture() {
-        // TODO: If the user has added a new profile picture then return true
-        return false;
+    private void openGallery() {
+        Intent gallery = new Intent(Intent.ACTION_PICK,
+            MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+    startActivityForResult(gallery, GALLERY_CODE);
     }
 
+    @Override
+    public void onActivityResult (int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == GALLERY_CODE) {
+            mImageUri = data.getData();
+            partnerEditorImageView.setImageURI(mImageUri);
+        }
+    }
 }
